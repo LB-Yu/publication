365b8be0bbef7163a99a9017be3a623294197bad
LiebingYu <liebingyu@gmail.com>
2019-09-14 21:08:09 +0800

    add gpu for ad

---
+++ gpu_for_ad/cmake-build-debug/CMakeFiles/c_runtime_api.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/code/LearningMap/3ds/ai_sys/CSE599W/code/assignment2-2017/src/c_runtime_api.cc" "D:/code/LearningMap/3ds/ai_sys/CSE599W/code/assignment2-2017/cmake-build-debug/CMakeFiles/c_runtime_api.dir/src/c_runtime_api.cc.obj"
+  "D:/code/LearningMap/3ds/ai_sys/CSE599W/code/assignment2-2017/src/cpu_device_api.cc" "D:/code/LearningMap/3ds/ai_sys/CSE599W/code/assignment2-2017/cmake-build-debug/CMakeFiles/c_runtime_api.dir/src/cpu_device_api.cc.obj"
+  "D:/code/LearningMap/3ds/ai_sys/CSE599W/code/assignment2-2017/src/cuda_device_api.cc" "D:/code/LearningMap/3ds/ai_sys/CSE599W/code/assignment2-2017/cmake-build-debug/CMakeFiles/c_runtime_api.dir/src/cuda_device_api.cc.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "C:/Program"
+  "../Files/NVIDIA"
+  "../GPU"
+  "../Computing"
+  "../Toolkit/CUDA/v9.0/include"
+  "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v9.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
